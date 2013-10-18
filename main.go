a36b6360e68185c58935dacd0824fc1683adc0c9
Matt Parker <mdparker89@gmail.com>
2013-10-18 14:22:38 -0400

    add confirmation email

--- main.go
+++ main.go
@@ -1,45 +1,85 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/ant0ine/go-urlrouter"
 	"github.com/streadway/amqp"
+	rbtmq "github.com/wurkhappy/Rabbitmq-go-wrapper"
+	"github.com/wurkhappy/WH-Email/handlers"
 	"log"
-	// "time"
 )
 
 var (
 	uri          = flag.String("uri", "amqp://guest:guest@localhost:5672/", "AMQP URI")
-	exchange     = flag.String("exchange", "test-exchange", "Durable, non-auto-deleted AMQP exchange name")
+	exchange     = flag.String("exchange", "email", "Durable, non-auto-deleted AMQP exchange name")
 	exchangeType = flag.String("exchange-type", "direct", "Exchange type - direct|fanout|topic|x-custom")
-	queue        = flag.String("queue", "test-queue", "Ephemeral AMQP queue name")
-	bindingKey   = flag.String("key", "test-key", "AMQP binding key")
+	queue        = flag.String("queue", "email", "Ephemeral AMQP queue name")
 	consumerTag  = flag.String("consumer-tag", "simple-consumer", "AMQP consumer tag (should not be blank)")
 )
+
+//order matters so most general should go towards the bottom
+var router urlrouter.Router = urlrouter.Router{
+	Routes: []urlrouter.Route{
+		urlrouter.Route{
+			PathExp: "/user/verify",
+			Dest: map[string]interface{}{
+				"POST": handlers.ConfirmSignup,
+			},
+		},
+	},
+}
 
 func init() {
 	flag.Parse()
 }
 
 func main() {
-
 	log.Printf("dialing %q", *uri)
 	conn, err := amqp.Dial(*uri)
 	if err != nil {
 		fmt.Errorf("Dial: %s", err)
 	}
-	_, err = NewConsumer(handle, conn, *exchange, *exchangeType, *queue, *bindingKey, *consumerTag)
+	c, err := rbtmq.NewConsumer(conn, *exchange, *exchangeType, *queue, *consumerTag)
 	if err != nil {
 		log.Fatalf("%s", err)
 	}
 
-	// _, err = NewConsumer(handle, conn, *exchange, *exchangeType, *queue, "other", *consumerTag)
-	// if err != nil {
-	// 	log.Fatalf("%s", err)
-	// }
+	deliveries := c.Consume(*queue)
+
+	err = router.Start()
+	if err != nil {
+		panic(err)
+	}
+	log.Print("route")
+	routeMapper(deliveries)
 	select {}
 }
 
+func routeMapper(deliveries <-chan amqp.Delivery) {
+	for d := range deliveries {
+		//this should use a goroutine but channel closes under heavy load
+		func(amqp.Delivery) {
+			route, params, err := router.FindRoute(d.RoutingKey)
+			if err != nil || route == nil {
+				log.Printf("first error is: %v", err)
+				return
+			}
 
+			var m map[string]interface{}
+			json.Unmarshal(d.Body, &m)
+			body := m["Body"].(map[string]interface{})
+			routedMap := route.Dest.(map[string]interface{})
+			handler := routedMap[m["Method"].(string)].(func(map[string]string, map[string]interface{}) error)
+			err = handler(params, body)
+			if err != nil {
+				log.Printf("second error is: %v", err)
+				d.Nack(false, true)
+			}
+			d.Ack(false)
+		}(d)
 
-
+	}
+	log.Printf("handle: deliveries channel closed")
+}
