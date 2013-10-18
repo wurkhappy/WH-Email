4ebd122ca867fa50bb7303744fe4cc7c7f9749b5
Matt Parker <mdparker89@gmail.com>
2013-10-18 14:22:43 -0400

    add confirmation email

---
+++ handlers/handlers.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/wurkhappy/mandrill-go"
+	"log"
+	"net/http"
+)
+
+func init() {
+	mandrill.APIkey = "tKcqIfanhMnYrTtGrDixBA"
+}
+
+func ConfirmSignup(params map[string]string, body map[string]interface{}) error {
+	userID := body["id"].(string)
+	email := body["email"].(string)
+	path := "/user/" + userID + "/verify"
+	signature := signURL(userID, path)
+	m := mandrill.NewCall()
+	m.Category = "messages"
+	m.Method = "send-template"
+	message := new(mandrill.Message)
+	message.GlobalMergeVars = append(message.GlobalMergeVars,
+		&mandrill.GlobalVar{Name: "signup_link", Content: "http://localhost:4000" + path + "?signature=" + signature},
+	)
+	message.To = []mandrill.To{{Email: email}}
+	m.Args["message"] = message
+	m.Args["template_name"] = "Confirm Email and Signup"
+	m.Args["template_content"] = []mandrill.TemplateContent{{Name: "blah", Content: "nfd;jd;fjvnbd"}}
+
+	resp, err := m.Send()
+	if err != nil {
+		return err
+	}
+
+	var requestData map[string]interface{}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	data := buf.Bytes()
+	json.Unmarshal(data, &requestData)
+	log.Print(requestData)
+	log.Print(err)
+	return nil
+}
+
+func signURL(userID, path string) string {
+	body := bytes.NewReader([]byte(`{"path":"` + path + `"}`))
+	r, _ := http.NewRequest("POST", "http://localhost:3000/user/"+userID+"/sign", body)
+	respData, _ := sendRequest(r)
+	signature := respData["signature"].(string)
+	return signature
+}
---
+++ handlers/utilities.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func sendRequest(r *http.Request) (map[string]interface{}, []byte) {
+	client := &http.Client{}
+	resp, err := client.Do(r)
+	if err != nil {
+		fmt.Printf("Error : %s", err)
+	}
+	respBuf := new(bytes.Buffer)
+	respBuf.ReadFrom(resp.Body)
+	var respData map[string]interface{}
+	json.Unmarshal(respBuf.Bytes(), &respData)
+	return respData, respBuf.Bytes()
+}
