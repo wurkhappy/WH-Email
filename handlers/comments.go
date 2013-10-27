5d7efa64cb01a21172b1654d049bec9f00dc8d57
Matt Parker <mdparker89@gmail.com>
2013-10-27 17:07:51 -0400

    send emails with new messages

---
+++ handlers/comments.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"encoding/json"
+	"github.com/wurkhappy/mandrill-go"
+	"time"
+)
+
+type Comment struct {
+	ID                 string    `json:"id"`
+	UserID             string    `json:"userID"`
+	AgreementID        string    `json:"agreementID"`
+	AgreementVersionID string    `json:"agreementVersionID"`
+	DateCreated        time.Time `json:"dateCreated"`
+	Text               string    `json:"text"`
+	MilestoneID        string    `json:"milestoneID"`
+	StatusID           string    `json:"statusID"`
+}
+
+func SendComment(params map[string]string, body map[string]*json.RawMessage) error {
+	var comment *Comment
+	json.Unmarshal(*body["comment"], &comment)
+	sender := getUserInfo(comment.UserID)
+	agreement := getAgreement(comment.AgreementVersionID)
+	var recipientID string = agreement.ClientID
+	if agreement.ClientID == comment.UserID {
+		recipientID = agreement.FreelancerID
+	}
+	recipient := getUserInfo(recipientID)
+
+	path := "/agreement/v/" + comment.AgreementVersionID
+	expiration := int(time.Now().Add(time.Hour * 24 * 5).Unix())
+	signatureParams := createSignatureParams(recipientID, path, expiration)
+	m := mandrill.NewCall()
+	m.Category = "messages"
+	m.Method = "send-template"
+	message := new(mandrill.Message)
+	message.GlobalMergeVars = append(message.GlobalMergeVars,
+		&mandrill.GlobalVar{Name: "AGREEMENT_LINK", Content: "http://localhost:4000" + path + "?" + signatureParams},
+		&mandrill.GlobalVar{Name: "AGREEMENT_NAME", Content: agreement.Title},
+		&mandrill.GlobalVar{Name: "SENDER_FULLNAME", Content: sender.getEmailOrName()},
+		&mandrill.GlobalVar{Name: "MESSAGE", Content: comment.Text},
+	)
+	message.To = []mandrill.To{{Email: recipient.Email, Name: recipient.createFullName()}}
+	m.Args["message"] = message
+	m.Args["template_name"] = "New Message"
+	m.Args["template_content"] = []mandrill.TemplateContent{{Name: "blah", Content: "nfd;jd;fjvnbd"}}
+
+	_, err := m.Send()
+	if err != nil {
+		return err
+	}
+	return nil
+}
