f2099b402af9645f1adda209eda648a5146b0915
MattParker89 <mdparker89@gmail.com>
2013-12-20 17:02:21 -0500

    fix compile errors

--- handlers/replies.go
+++ handlers/replies.go
@@ -8,13 +8,30 @@ import (
 	"github.com/garyburd/redigo/redis"
 	"github.com/wurkhappy/WH-Config"
 	"io"
-	"net/url"
 )
 
 func ProcessReply(params map[string]string, body map[string]*json.RawMessage) error {
 	var str string
 	json.Unmarshal(*body["message"], &str)
 	fmt.Println(str)
+	return nil
+	r := bytes.NewReader([]byte("msg.(string)"))
+	text := parseHtml(r)
+	c := redisPool.Get()
+	commentBytes, _ := redis.Bytes(c.Do("GET", "message_id"))
+	var comment *Comment
+	json.Unmarshal(commentBytes, &comment)
+	newComment := new(Comment)
+	newComment.AgreementID = comment.AgreementID
+	newComment.Tags = comment.Tags
+	newComment.Text = text
+	newComment.UserID = comment.RecipientID
+
+	newCommentjson, _ := json.Marshal(newComment)
+	_, statusCode := sendServiceRequest("POST", config.CommentsService, "/agreement/"+comment.AgreementID+"/comments", newCommentjson)
+	if statusCode >= 400 {
+		return nil
+	}
 	return nil
 }
 
